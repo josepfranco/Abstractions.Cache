d4c98db51f231ce6a3ca1a047ae751c27fceda23
José Franco <josepgvf@gmail.com>
2020-08-09 13:43:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Abstractions.Cache
+Boilerplate abstractions and contracts for all cache operations in C#.
---
+++ src/Abstractions.Cache/ICache.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Abstractions.Cache
+{
+    /// <summary>
+    /// Contract for a caching service.
+    /// Agnostic whether it's used for an in-Memory cache or a distributed cache.
+    /// </summary>
+    public interface ICache
+    {
+        /// <summary>
+        /// Gets an object saved in a cache with a certain key asynchronously
+        /// </summary>
+        /// <param name="key">the key</param>
+        /// <typeparam name="TObject">the object type it's expecting to get</typeparam>
+        /// <returns>a task with the object deserialized, or null if none was found</returns>
+        Task<TObject> GetAsync<TObject>(string key);
+        
+        /// <summary>
+        /// Saves an object in a cache with a certain key and expiry duration asynchronously
+        /// </summary>
+        /// <param name="key">the key</param>
+        /// <param name="value">the object to save</param>
+        /// <param name="expiry">when this cache is supposed to expire</param>
+        /// <returns>a task with whether the value was successfully inserted or not</returns>
+        Task<bool> SetAsync(string key, object value, TimeSpan? expiry = null);
+    }
+}
